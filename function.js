f0f984cb0684c1445eb2a42490c1e556e575203a
Akshat Ajmera <139946232+akshatajmera7@users.noreply.github.com>
2024-12-30 14:12:28 +0530

    Update function.js

--- function.js
+++ function.js
@@ -2,7 +2,7 @@ const daysEl = document.getElementById("days");
 const hoursEl = document.getElementById("hours");
 const minutesEl = document.getElementById("minutes");
 const secondsEl = document.getElementById("seconds");
-const newYear = "17 Jan 2025";
+const newYear = "1 Jan 2025";
 function countdown() {
   const newYearDate = new Date(newYear);
   const currentDate = new Date();
